1548a55cd8df76758928bf6bbdcf251fffe6ae3e
benleahy91 <benleahy91@gmail.com>
2019-03-04 14:43:55 -0500

    added other counter actions

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -6,9 +6,25 @@ const reducer = (state = initialState, action) => {
 	if (action.type === 'INCREMENT') {
 		return {
 			counter: state.counter + 1
-		}
-	}
+		};
+	};
+	if (action.type === 'DECREMENT') {
+		return {
+			counter: state.counter - 1
+		};
+	};
+	if (action.type === 'ADD') {
+		return {
+			counter: state.counter + 10
+		};
+	};
+	if (action.type === 'SUBTRACT') {
+		return {
+			counter: state.counter - 10
+		};
+	};
 	return state;
+
 };
 
 export default reducer;
