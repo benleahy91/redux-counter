c6f824128dc92d1e3e703e47aea7fe29b9375228
benleahy91 <benleahy91@gmail.com>
2019-03-12 14:06:34 -0400

    updated reducers and actions

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -8,8 +8,9 @@ export const saveResult = (res) => {
 };
 
 export const storeResult = (res) => {
-	return dispatch => {
+	return (dispatch, getState) => {
 		setTimeout(() => {
+			const oldCounter = getState().ctr.counter;			
 			dispatch(saveResult(res));
 		}, 2000);
 	};
